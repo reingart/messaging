bedb2d36ad4fac5dcfd07f8f07abe81421c1c2c0
Mariano Reingart <reingart@gmail.com>
2021-11-28 22:09:40 -0300

    Redis stream consumer (async)

--- redis_async.py
+++ redis_async.py
@@ -1,5 +1,6 @@
 import asyncio
 import os
+import sys
 import uuid
 from time import sleep, time
 
@@ -9,7 +10,7 @@ MAX_MESSAGES = int(os.environ.get("MESSAGES", "100000"))
 SIZE = int(os.environ.get("MESSAGE_SIZE", "1024"))
 
 
-async def main(max_messages, size, producer="prod", stream_key="test"):
+async def producer(max_messages, size, producer="prod", stream_key="test"):
     # Redis client bound to single connection (no auto reconnection).
     redis = aioredis.from_url(
         "redis://localhost", encoding="utf-8", decode_responses=True
@@ -35,7 +36,34 @@ async def main(max_messages, size, producer="prod", stream_key="test"):
                 print(count, resp[-1], int(count/elapsed), "m/s")
 
 
+async def consumer(max_messages, stream_key="test"):
+    # Redis client bound to single connection (no auto reconnection).
+    redis = aioredis.from_url(
+        "redis://localhost", encoding="utf-8", decode_responses=True
+    )
+    async with redis.client() as conn:
+        last_id = 0
+        sleep_ms = 5000
+        count = 0
+        start = time()
+        while count < max_messages:
+            resp = await conn.xread(
+                {stream_key: last_id}, count=1000, block=sleep_ms
+            )
+            if resp:
+                key, messages = resp[0]
+                last_id, data = messages[-1]
+                count += len(messages)
+                elapsed = time() - start
+                print(count, last_id, int(count/elapsed), "m/s")
+        return count
+
 if __name__ == "__main__":
     start = time()
-    asyncio.run(main(MAX_MESSAGES, SIZE))
-    print("REDIS %d messages added - Elapsed: %.3f secs" % (MAX_MESSAGES, time() - start))
+    if '--consumer' in sys.argv:
+        print("Consuming stream...")
+        asyncio.run(consumer(MAX_MESSAGES))
+    else:
+        print("Producing stream...")
+        asyncio.run(producer(MAX_MESSAGES, SIZE))
+    print("REDIS %d messages - Elapsed: %.3f secs" % (MAX_MESSAGES, time() - start))
